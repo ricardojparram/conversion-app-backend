32ef2d81f0cd8323149fcd61ab850f9c960ca053
ricardojparram <ricardo.prxr16@gmail.com>
2025-08-05 20:59:11 -0400

    feat: curreciens and exchange_rates table

---
+++ database/migrations/2025_08_06_005719_create_exchange_rates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('currency_id'); // Relación con currencies
+            $table->decimal('rate', 12, 4); // Tasa de cambio
+            $table->datetime('date'); // Fecha y hora de la tasa
+            $table->timestamps();
+
+            $table->foreign('currency_id')->references('id')->on('currencies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
