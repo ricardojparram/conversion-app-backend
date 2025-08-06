e5e11edffb01d17435fa4c1986dca97f44a7defa
ricardojparram <ricardo.prxr16@gmail.com>
2025-08-05 23:36:24 -0400

    feat: command and scheduling

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Artisan::call('scrape:exchange-rates');
+})->dailyAt('8:00')->onFailure(function () {
+    // Handle failure, e.g., log the error or notify the admin
+    Log::error('Failed to scrape exchange rates.');
+});
+
+Artisan::command('scrape:exchange-rates', function () {
+    dispatch(new \App\Jobs\ScrapeExchangeRatesJob());
+});
